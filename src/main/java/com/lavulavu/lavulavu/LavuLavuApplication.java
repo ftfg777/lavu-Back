f04a2d9318f44f93ee0bf4c7e35823fcd5450ed5
ftfg777 <ftfgz5093@gmail.com>
2024-12-21 18:50:02 +0900

    [#1] init: new project

---
+++ src/main/java/com/lavulavu/lavulavu/LavuLavuApplication.java
@@ -0,0 +1,13 @@
+package com.lavulavu.lavulavu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavuLavuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavuLavuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavulavu/lavulavu/LavuLavuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavulavu.lavulavu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavuLavuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
